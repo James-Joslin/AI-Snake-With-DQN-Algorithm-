9d12896533b493e24d9d836b18910267a8f30bb9
James Joslin <74412992+James-Joslin@users.noreply.github.com>
2021-05-07 14:06:51 +0100

    Update Snake_Agent.py

--- Snake_Agent.py
+++ Snake_Agent.py
@@ -8,13 +8,15 @@ from Model import Linear_QNet, QTrainer
 MAX_MEMORY = 100_000
 BATCH_SIZE = 1000
 LR = 0.001
+EPSILON = 0.0
+GAMMA = 0.9
 
 class Agent:
 
     def __init__(self):
         self.n_games = 0
-        self.epsilon = 0 # randomness
-        self.gamma = 0.9 # discount rate
+        self.epsilon = EPSILON # randomness
+        self.gamma = GAMMA # discount rate
         self.memory = deque(maxlen=MAX_MEMORY) # popleft()
         self.model = Linear_QNet(11, 256, 3)
         self.trainer = QTrainer(self.model, lr=LR, gamma=self.gamma)
